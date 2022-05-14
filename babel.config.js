64db798f19916ddac4abd23bd0fe912cd90f26ef
firstChairCoder <md1qq7@gmail.com>
2022-05-14 01:48:47 +0100

    chore: enable absolute path resolution

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,30 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.android.js',
+            '.ios.js'
+          ],
+          root: ['./'],
+          alias: {
+            '@assets': './assets',
+            '@components': './src/components',
+            '@utils': './src/utils',
+            '@env': './src/env',
+            '@hooks': './src/hooks',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@redux': './src/redux',
+            '@context': './src/context'
+          },
+        },
+      ]
+    ]
   };
 };
