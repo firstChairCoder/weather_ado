60312ebd16edb35300a6d0f151933408395178d4
firstChairCoder <md1qq7@gmail.com>
2022-05-14 09:28:39 +0100

    chore: add custom fonts nunito and source serif pro

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  Nunito_300Light,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  useFonts
+} from "@expo-google-fonts/nunito";
+import {
+  SourceSerifPro_600SemiBold,
+  SourceSerifPro_700Bold
+} from "@expo-google-fonts/source-serif-pro";
+import AppLoading from "expo-app-loading";
 import { RootNavigator } from "@navigation";
 
+LogBox.ignoreLogs([
+  "expo-app-loading is deprecated in favor of expo-splash-screen"
+]);
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Light: Nunito_300Light,
+    Regular: Nunito_400Regular,
+    Medium: Nunito_500Medium,
+    SemiBold: SourceSerifPro_600SemiBold,
+    Bold: SourceSerifPro_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <SafeAreaProvider>
       <StatusBar style="inverted" />
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export const OnboardingScreen = () => {
-  return <View style={{ flex: 1, backgroundColor: "lime" }} />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "papayawhip",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Text style={{ fontSize: 32, fontFamily: "Bold" }}>Blues</Text>
+      <Text style={{ fontSize: 16, fontFamily: "Light" }}>Clues</Text>
+    </View>
+  );
 };
